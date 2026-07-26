f79f09197393faa32baa923fb6ee48fbc06f62ac
SantojeetChakraborty <santojeetcharaborty@gmail.com>
2026-07-26 13:47:05 +0530

    (feat) : Added back navigation button in Categories page

--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -16,7 +16,7 @@ function CategoriesPage() {
         <PageHeader
           title="Categories"
           subtitle="Browse all available categories"
-          showBack={false}
+          showBack
         />
 
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-8">
