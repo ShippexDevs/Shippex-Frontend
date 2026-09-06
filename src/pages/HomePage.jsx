8d7a4817ce9d6b41ff90873b047bf8a21e5b271c
SantojeetChakraborty <santojeetchakraborty@gmail.com>
2026-09-06 09:29:33 +0530

    Enhanced user UI/UX

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
 import MobileLayout from "../layouts/MobileLayout";
-import Header from "../components/home/Header";
 import DeliveryCard from "../components/home/Deliverycard";
 import SearchBar from "../components/common/Searchbar";
 import CategoriesSection from "../components/home/CategoriesSection";
@@ -53,13 +52,6 @@ function HomePage() {
     <MobileLayout>
 
       <div className="bg-[#F5F8FA]">
-
-        {/* Keep this only if Header contains
-            home-specific content.
-            If it is another navigation/header,
-            remove it. */}
-
-        
 
         <main className="mx-auto max-w-7xl space-y-8 px-4 pt-6 sm:px-6 lg:px-8">
 
